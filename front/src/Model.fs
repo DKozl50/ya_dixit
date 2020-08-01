68d21d64a03c01ca4267e1bcfcc16a8513add193
OperatorGK <operator.gk@gmail.com>
2020-08-01 22:57:36 +0300

    Imaginarium Model

--- front/src/Model.fs
+++ front/src/Model.fs
@@ -1,29 +1,36 @@
 module Model
 
 [<RequireQualifiedAccess>]
-type PlayerSide =
-    | Cross
-    | Nought
+type PlayerRole =
+    | Storyteller
+    | Listener
+    | None
+
+type Player =
+    { Name: string
+      Role: PlayerRole
+      Score: int
+      MadeMove: bool }
 
 [<RequireQualifiedAccess>]
-type GameCell =
-    | Empty
-    | Cross
-    | Nought
-
-[<RequireQualifiedAccess>]
-type GameProgress =
+type GamePhase =
     | Waiting
-    | CrossTurn
-    | NoughtTurn
-    | CrossWin
-    | NoughtWin
-    | Draw
+    | Storytelling
+    | Matching
+    | Guessing
+    | Interlude
+    | Victory of Player
+
+type Hand = { Cards: int list; SelectedCard: int }
+
+type Table =
+    { Cards: (int * Player option) list
+      Story: string }
 
 type GameState =
-    { Progress: GameProgress
-      Field: GameCell list // Exactly 9 elements
-      Side: PlayerSide }
+    { Players: Player list // the client player object is always on top
+      Hand: Hand
+      Table: Table }
 
 [<RequireQualifiedAccess>]
 type ModelState =
@@ -36,7 +43,9 @@ type UserMessage =
     | CreateRoom
     | JoinRoom of id: string
     | LeaveRoom
-    | MakeMove of cell: int
+    | TellStory of cardID: int * story: string
+    | DealCard of cardID: int
+    | GuessCard of cardID: int
 
 [<RequireQualifiedAccess>]
 type ServerMessage =
--- front/src/Update.fs
+++ front/src/Update.fs
@@ -3,35 +3,9 @@ module Update
 open Elmish
 open Model
 
-let cellOfSide =
-    function
-    | PlayerSide.Cross -> GameCell.Cross
-    | PlayerSide.Nought -> GameCell.Nought
-
-let switchTurn =
-    function
-    | GameProgress.CrossTurn -> GameProgress.NoughtTurn
-    | GameProgress.NoughtTurn -> GameProgress.CrossTurn
-    | x -> x
-
-let inline listUpdate (i: int) (x: 'T) (li: 'T list) =
-    let a = List.toArray li
-    a.[i] <- x
-    Array.toList a
-
-let inline konst x y = x
-
-let makeMove (x: int) (gs: GameState) =
-    let field' =
-        gs.Field |> listUpdate x (cellOfSide gs.Side)
-
-    { gs with
-          Field = field'
-          Progress = switchTurn gs.Progress }
-
-let updateGameState (updater: GameState -> GameState) (state: ModelState) =
+let updateGameState (newState: GameState) (state: ModelState) =
     match state with
-    | ModelState.Room (id, s) -> ModelState.Room(id, updater s)
+    | ModelState.Room (id, s) -> ModelState.Room(id, newState)
     | _ -> state
 
 let userUpdate (msg: UserMessage) (state: ModelState) =
@@ -40,12 +14,12 @@ let userUpdate (msg: UserMessage) (state: ModelState) =
     | UserMessage.CreateRoom -> ModelState.Connecting, Cmd.none
     | UserMessage.JoinRoom id -> ModelState.Connecting, Cmd.none
     | UserMessage.LeaveRoom -> ModelState.Lobby, Cmd.none
-    | UserMessage.MakeMove x -> updateGameState (makeMove x) state, Cmd.none
+    | _ -> state, Cmd.none
 
 let serverUpdate (msg: ServerMessage) (state: ModelState) =
     match msg with
     | ServerMessage.RoomConnect (id, s) -> ModelState.Room(id, s), Cmd.none
-    | ServerMessage.RoomUpdate ns -> updateGameState (konst ns) state, Cmd.none
+    | ServerMessage.RoomUpdate ns -> updateGameState ns state, Cmd.none
     | ServerMessage.FailConnect -> ModelState.Lobby, Cmd.none
 
 let update (msg: Msg) (state: ModelState) =
