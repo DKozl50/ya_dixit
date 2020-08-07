35a85f9265d3ea9496e255205b931465a749a975
OperatorGK <operator.gk@gmail.com>
2020-08-07 16:39:42 +0300

    Formatting

--- front/src/Lobby.fs
+++ front/src/Lobby.fs
@@ -19,45 +19,37 @@ let private sendJoinMessage dispatch name =
         |> dispatch
 
 let private lobbyButtonControl dispatch (refName: RefT) (refID: RefT) =
-    Bulma.button.button
-        [ color.isPrimary
-          prop.text "Погнали"
-          prop.onClick (fun _ ->
-              let name = refName.current.Value.value
-              let id = refID.current.Value.value
-              sendJoinMessage dispatch name id) ]
+    Bulma.button.button [ color.isPrimary
+                          prop.text "Погнали"
+                          prop.onClick (fun _ ->
+                              let name = refName.current.Value.value
+                              let id = refID.current.Value.value
+                              sendJoinMessage dispatch name id) ]
     |> Bulma.control.div
 
 let private lobbyInputIdControl (ref': RefT) =
-    Bulma.control.div
-        [ control.isExpanded
-          prop.children
-              [ Bulma.input.text
-                  [ prop.placeholder "ID комнаты (оставьте пустым для создания своей)"
-                    prop.ref ref' ] ] ]
+    Bulma.control.div [ control.isExpanded
+                        prop.children
+                            [ Bulma.input.text [ prop.placeholder "ID комнаты (оставьте пустым для создания своей)"
+                                                 prop.ref ref' ] ] ]
 
 let private lobbyInputNameControl (ref': RefT) =
-    Bulma.field.div
-        [ spacing.mx4
-          spacing.my4
-          prop.style [ style.width (length.em 33) ]
-          prop.children
-              [ Bulma.control.div
-                  [ control.isExpanded
-                    prop.children
-                        [ Bulma.input.text
-                            [ prop.placeholder "Введите свой никнейм"
-                              prop.ref ref' ] ] ] ] ]
+    Bulma.field.div [ spacing.mx4
+                      spacing.my4
+                      prop.style [ style.width (length.em 33) ]
+                      prop.children
+                          [ Bulma.control.div [ control.isExpanded
+                                                prop.children
+                                                    [ Bulma.input.text [ prop.placeholder "Введите свой никнейм"
+                                                                         prop.ref ref' ] ] ] ] ]
 
 let private lobbyInputControl dispatch (refName: RefT) (refID: RefT) =
-    Bulma.field.div
-        [ field.hasAddons
-          spacing.mx4
-          spacing.my4
-          prop.style [ style.width (length.em 33) ]
-          prop.children
-              [ lobbyInputIdControl refID
-                lobbyButtonControl dispatch refName refID ] ]
+    Bulma.field.div [ field.hasAddons
+                      spacing.mx4
+                      spacing.my4
+                      prop.style [ style.width (length.em 33) ]
+                      prop.children [ lobbyInputIdControl refID
+                                      lobbyButtonControl dispatch refName refID ] ]
 
 
 let private lobbyInputCompoment =
@@ -69,17 +61,14 @@ let private lobbyInputCompoment =
           lobbyInputControl dispatch refName refID ])
 
 let private lobbyTitle =
-    Bulma.title.h1
-        [ prop.text "Крестики-Нолики"
-          prop.style [ style.textAlign.center ]
-          spacing.my5
-          title.is3 ]
+    Bulma.title.h1 [ prop.text "Imaginarium"
+                     prop.style [ style.textAlign.center ]
+                     spacing.my5
+                     title.is3 ]
 
 let renderLobby dispatch =
     Bulma.card
-        [ Bulma.cardContent
-            [ spacing.px0
-              spacing.py0
-              prop.children
-                  [ lobbyTitle
-                    lobbyInputCompoment {| dispatch = dispatch |} ] ] ]
+        [ Bulma.cardContent [ spacing.px0
+                              spacing.py0
+                              prop.children [ lobbyTitle
+                                              lobbyInputCompoment {| dispatch = dispatch |} ] ] ]
--- front/src/Socket.fs
+++ front/src/Socket.fs
@@ -3,12 +3,13 @@ module Socket
 open Browser
 open Thoth.Json
 
-let socket = WebSocket.Create("wss://ya-dixit.herokuapp.com/socket")
+let private socket =
+    WebSocket.Create("wss://ya-dixit.herokuapp.com/socket")
 
-let sendJson (json: string) = socket.send(json)
+let sendJson (json: string) = socket.send (json)
 
-let inline sendObject (x: 'T) = Encode.Auto.toString(4, x) |> sendJson
-let parseServerMessage (json: string) : Result<Model.ServerMessage, string> = Decode.Auto.fromString(json)
+let inline sendObject (x: 'T) = Encode.Auto.toString (4, x) |> sendJson
+let parseServerMessage (json: string): Result<Model.ServerMessage, string> = Decode.Auto.fromString (json)
 
 let serverMessageSubscription (_: Model.ModelState) =
     let sub dispatch =
@@ -16,5 +17,7 @@ let serverMessageSubscription (_: Model.ModelState) =
             match s.data.ToString() |> parseServerMessage with
             | Ok msg -> Model.Msg.ServerMsg msg |> dispatch
             | Error s -> printfn "%A" s
+
         socket.onmessage <- f
+
     Elmish.Cmd.ofSub sub
