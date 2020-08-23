ded2dd7d6f600428581a697a320054e682e723c9
OperatorGK <operator.gk@gmail.com>
2020-08-23 17:34:18 +0300

    Fix

--- front/src/Init.fs
+++ front/src/Init.fs
@@ -3,34 +3,4 @@ module Init
 open Elmish
 open Model
 
-let examplePlayer = {
-    Name = "Саша <3"
-    Role = PlayerRole.Listener
-    Score = 0
-    MoveAvailable = false
-}
-
-let exampleOptionalCardInfo = {
-    Owner = examplePlayer
-    Voters = [ examplePlayer ; examplePlayer ]
-}
-
-let exampleHand = {
-    Cards = [ "linal"; "anal"; "karnaval"]
-    SelectedCard = Some "linal"
-}
-
-let exampleTable = {
-    Cards = [ "nol", None; "odin", Some exampleOptionalCardInfo ]
-    Story = Some "kollokvium"
-}
-
-let exampleGameState = {
-    Client = examplePlayer
-    Opponents = []
-    Hand = exampleHand
-    Table = exampleTable
-    Phase = GamePhase.Victory
-}
-
-let initModel () = ModelState.Room("id", exampleGameState) , Cmd.none
+let initModel () = ModelState.Lobby , Cmd.none
