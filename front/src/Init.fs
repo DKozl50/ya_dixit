0080a4b2ff214e93d292c401cfb2a313793b2647
OperatorGK <operator.gk@gmail.com>
2020-08-24 16:32:26 +0300

    Client-side support for initial state templating

--- front/src/Init.fs
+++ front/src/Init.fs
@@ -3,4 +3,26 @@ module Init
 open Elmish
 open Model
 
-let initModel () = ModelState.Lobby , Cmd.none
+let initState =
+    let x =
+        Browser.Dom.document.getElementById("initialState").innerText
+        |> Thoth.Json.Decode.Auto.fromString<ModelState>
+
+    match x with
+    | Ok state -> state
+    | Error e ->
+        eprintfn "Error parsing initial state:\n%A" e
+        ModelState.Lobby
+
+let initMessage =
+    let x =
+        Browser.Dom.document.getElementById("initialMessage").innerText
+        |> Thoth.Json.Decode.Auto.fromString<Msg>
+
+    match x with
+    | Ok msg -> Cmd.ofMsg msg
+    | Error e ->
+        eprintfn "Error parsing initial message:\n%A" e
+        Cmd.none
+
+let initModel () = initState, initMessage
